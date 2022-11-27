b420cfb290e36de260cf2626f34ad051cfb24a77
Georgios Atmatzidis <gatmatzidi@gmail.com>
2022-11-27 03:03:44 +0200

    FCFS final implementation

--- fcfs.c
+++ fcfs.c
@@ -7,38 +7,6 @@ typedef struct {
 	int burst_time;
 } process;
 
-void swap(process* x, process* y)
-{
-    process temp = *x;
-    *x = *y;
-    *y = temp;
-}
-
-process* sortByArrival(process* arr, int n)
-{
-    //Copy arr contents to another array named routing.
-    process* routing;
-    routing = malloc(n*sizeof(process));
-    for (int i=0;i<n;i++)
-    {
-        routing[i].pid = arr[i].pid;
-        routing[i].arrival_time = arr[i].arrival_time;
-        routing[i].burst_time = arr[i].burst_time;
-    }
-    //Bubble sort by arrival time the routing array.
-    for (int i=0;i<n;i++)
-    {
-        for (int j=0;j<n-i;j++)
-        {
-            if (routing[j].arrival_time > routing[j+1].arrival_time)
-            {
-                swap(&routing[j],&routing[j+1]);
-            }
-        }
-    }
-    return routing;
-}
-
 int main() {
 
 	/* read in data - DO NOT EDIT (START) */
@@ -53,21 +21,35 @@ int main() {
 		scanf("%d", &arr[i].arrival_time);
 		scanf("%d", &arr[i].burst_time);
 	}
-	/* read in data - DO NOT EDIT (END) */
-//    for (int i=0;i<n;i++)
-//    {
-//        printf("%d - %d - %d\n",arr[i].pid, arr[i].arrival_time, arr[i].burst_time);
-//    }
+    //Calculate total burst time for all processes.
     int total_time=0;
     for (int i=0;i<n;i++)
     {
         total_time += arr[i].burst_time;
     }
-    process *routing=malloc(n*sizeof(process));
-    routing = sortByArrival(arr,n);
+    //Copy processes data to a new array named routing for data integrity.
+    process *routing;
+    routing = malloc(n*sizeof(process));
     for (int i=0;i<n;i++)
     {
-        printf("%d - %d - %d\n",routing[i].pid, routing[i].arrival_time, routing[i].burst_time);
+        routing[i].pid = arr[i].pid;
+        routing[i].arrival_time = arr[i].arrival_time;
+        routing[i].burst_time = arr[i].burst_time;
+    }
+    // FCFS - First Come First Served
+    int pid_counter = 0; //Counter that keeps track of the current process in CPU
+    for (int i=0;i<total_time;i++)
+    {
+        //If a process has ended.
+        if (routing[pid_counter].burst_time == 0)
+        {
+            //Move to the next process.
+            pid_counter++;
+        }
+        //Print the current process in CPU
+        printf("%d\n",routing[pid_counter].pid);
+        //Decrease by one burst time of current process in CPU
+        routing[pid_counter].burst_time--;
     }
 	return 0; /* DO NOT EDIT THIS LINE */
 }
